7449cb7c3e167190a7784ad464f9bdbc536e33cf
Jeongwoo <fundhaa@gmail.com>
2023-01-10 23:34:13 +0900

    python study 230110_2

--- 5-2.쇠막대기.py
+++ 5-2.쇠막대기.py
@@ -1,5 +1,18 @@
 import sys
 sys.stdin = open("input.txt", "rt")
-m = str(input())
-stack = list(m.split())
-print(stack)
+m = input()
+stack = []
+cnt = 0
+for i in range(len(m)):
+    if m[i]=="(":
+        stack.append(m[i])
+
+    elif m[i]==")" and m[i-1]=="(":
+        stack.pop()
+        cnt+=len(stack)
+
+    elif m[i]==")" and m[i-1]!="(":
+        cnt+=1
+        stack.pop()
+
+print(cnt)
