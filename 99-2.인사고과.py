0d59880b3e33a72f3bd85f2c4b6fb07fe54d811a
unknown <fundhaa@gmail.com>
2023-01-26 02:04:27 +0900

    python study 230125_3

--- 99-2.인사고과.py
+++ 99-2.인사고과.py
@@ -3,12 +3,22 @@ def solution(scores):
 
     # 일단 인센티브를 받을 수 있는지 없는지
     # 인센티브를 못받는 경우 리스트에서 제외
+    '''
+    a = dict()
+    i=1
+    for x in scores:
+        a[i]=x[0]+x[1]
+        i+=1
+    print(a)
+'''
     for x in scores:
         i = 0
         while True:
             if i >= len(scores) - 1:
                 break
             if x[0] < scores[i][0] and x[1] < scores[i][1]:
+                if x[0] == scores[0][0] and x[1] == scores[0][1]:
+                    return -1
                 scores.remove(x)
                 break
             i += 1
@@ -22,5 +32,4 @@ def solution(scores):
     ans.sort(reverse=True)
     answer = ans.index(tmp) + 1
     print(answer)
-
     return answer
