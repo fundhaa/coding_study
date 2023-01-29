cb571e397fbcf0f333a10bba185189c3cc3d2be8
Jeongwoo <fundhaa@gmail.com>
2023-01-29 22:49:24 +0900

    python study 230129_3

--- 5-11.최대힙.py
+++ 5-11.최대힙.py
@@ -1,2 +1,16 @@
 import sys
 import heapq as hq
+sys.stdin = open("input.txt", "rt")
+
+ans = []
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    elif n == 0:
+        if len(ans)==0:
+            print(-1)
+        else:
+            print(-hq.heappop(ans))
+    else:
+        hq.heappush(ans, -n)
