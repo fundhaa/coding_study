d33c933684ae8728f0d536e07c871c658fe3e158
Jeongwoo <fundhaa@gmail.com>
2023-01-30 00:38:57 +0900

    python study 230129_5

--- 6-1.재귀함수를 이용한 이진수출력.py
+++ 6-1.재귀함수를 이용한 이진수출력.py
@@ -10,8 +10,16 @@ def dv(a, ans):
         ans = ans[::-1]
         print(ans)
 
+def DFS(x):
+    if x<=0:
+        return
+    else:
+        DFS(x//2)
+        print(x%2, end="")
+
 if __name__ == "__main__":
     n = int(input())
     ans = ""
     dv(n, ans)
+    DFS(n)
 
