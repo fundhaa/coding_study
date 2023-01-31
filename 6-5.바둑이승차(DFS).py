e69070fd5ad5074fd4ebccebaac6c0506403d96d
Jeongwoo <fundhaa@gmail.com>
2023-02-01 00:32:24 +0900

    python study 230131_3

--- 6-5.바둑이승차(DFS).py
+++ 6-5.바둑이승차(DFS).py
@@ -2,11 +2,13 @@ import sys
 sys.stdin = open("input.txt", "rt")
 
 def DFS(v, sum):
+    global res # main 함수의 res 가져오기
     if sum>c:
         return
     else:
         if sum>ans[0]:
             ans[0]=sum
+            res=sum
     if v>=n:
         return
     else:
@@ -17,7 +19,9 @@ if __name__ == "__main__":
     c, n = map(int, input().split())
     arr = []
     ans=[0]
+    res = 0
     for _ in range(n):
         arr.append(int(input()))
     DFS(0, 0)
     print(ans[0])
+    print(res)
