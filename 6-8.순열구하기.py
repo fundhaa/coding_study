cc2ff3912f94b0f492d544f96b17c76a84ad8959
Jeongwoo <fundhaa@gmail.com>
2023-02-07 15:23:58 +0900

    python study 230207

--- 6-8.순열구하기.py
+++ 6-8.순열구하기.py
@@ -27,15 +27,40 @@ def DFS(v):
             DFS(v+1)
             arr1[i]=0
 
+def dfss(node):
+    global cnt
+    if node>=m:
+        for x in arr2:
+            print(x, end= " ")
+        print()
+        cnt+=1
+        return
+    else:
+        for i in range(1, n+1):
+            if chk[i]==0:
+                arr2[node]=i
+                chk[i] = 1
+                dfss(node+1)
+                chk[i]=0
+
+
+
+
 if __name__ == "__main__":
     start = time.time()
     n, m = map(int, input().split())
     arr = [0]*m
     arr1 = [0]*(n+1)
+    arr2 = [0]*m
+    chk = [0]*(n+1)
+    cnt=0
     dfs(0, -1)
     print()
     tmp=0
     DFS(0)
+    print()
+    dfss(0)
+    print(cnt)
 
     print("time : ", time.time()-start)
 
